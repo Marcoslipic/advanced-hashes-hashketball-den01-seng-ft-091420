d9d07059d0273142c4225c3d16fa43ed1e3b6cf1
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 18:33:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,12 @@ end
 # Write code here
 
 def num_points_scored (player_name)
-  game_hash.each do |location, team_data|
-    team_data.each do 
+  game_hash.each do |home_or_away, team_data|
+    if team_data == [:players]
+      players.each do |player|
+        return player 
+      end 
+    end 
   end 
 end 
 
