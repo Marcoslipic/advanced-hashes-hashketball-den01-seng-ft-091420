d8d40a5b0af907e2bb86acb3c4e8d6875554c581
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-02 19:56:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ def player_stats (player_name_search)
     team_data.each do |key, value|
       if key == :player 
         value.each do |player|
-          if input == player[:player_name]
+          if player_name_search == player[:player_name]
             player.delete(:player_name)
             return player
       end 
